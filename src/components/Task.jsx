b053bfea0ab301c9d1af55190ff974cbc4106d28
Oleksii <oleksiy@noroutine.me>
2021-05-18 16:13:46 +0200

    Revert and change to jsx

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -18,7 +18,6 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           value={title}
           readOnly={true}
           placeholder="Input title"
-          style={{ background: 'red' }}
         />
       </div>
 
