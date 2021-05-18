6ab8df98b4a514bd14ff1fa657b71c6ad997531c
Oleksii <oleksiy@noroutine.me>
2021-05-18 16:23:56 +0200

    Elipsis text truncate

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -18,6 +18,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           value={title}
           readOnly={true}
           placeholder="Input title"
+          style={{ textOverflow: 'ellipsis' }}
         />
       </div>
 
